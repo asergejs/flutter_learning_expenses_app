96efc1b46adf791e879e0026944f93ee280575c8
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2020-10-11 14:27:14 +0300

    93 - Closing modal, fixing disappearing input

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 350,
+      height: 500,
       child: ListView.builder(
         //89
         itemBuilder: (ctx, idx) {

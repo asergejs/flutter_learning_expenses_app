91d6baa2a1210a3106637cee2fe92531a9a25a86
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2020-10-11 13:59:48 +0300

    92 - Showing a modal screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:expenses/widgets/user_transactions.dart';
+import 'package:expenses/widgets/new_transaction.dart';
+import 'package:expenses/widgets/transaction_list.dart';
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,7 +15,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> _userTransactions = [
+    Transaction(id: 't1', title: 'beer', amount: 69.99, date: DateTime.now()),
+    Transaction(id: 't2', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't3', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't4', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't5', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't6', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't7', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't8', title: 'car', amount: 177.7, date: DateTime.now()),
+    Transaction(id: 't9', title: 'car', amount: 177.7, date: DateTime.now()),
+  ];
+
+  void _addTransaction(String title, double amount) {
+    final newTx = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(context: ctx, builder: (_) {
+      return NewTransaction(_addTransaction);
+    }, );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +60,7 @@ class MyHomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
           )
         ],
       ),
@@ -42,14 +80,14 @@ class MyHomePage extends StatelessWidget {
                 ),
               )
             ]),
-            UserTransactions()
+            TransactionList(_userTransactions)
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, //91
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ), //91
     );
   }

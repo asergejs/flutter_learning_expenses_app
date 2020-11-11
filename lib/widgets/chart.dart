e23c901d5d5a7763e3ee24af296411155376efa3
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2020-11-11 22:35:44 +0200

    102 - populating chart with values

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import '../widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
-
   final List<Transaction> recentTransactions;
 
   Chart(this.recentTransactions);
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index),); //98
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      ); //98
       double totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
@@ -21,9 +23,18 @@ class Chart extends StatelessWidget {
         }
       }
 
-      return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'amount': 9.99}; //98,100(5:33)
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      }; //98,100(5:33)
     });
   } //98
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +44,8 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return Text('${data['day']} : ${data['amount']}');
+          return ChartBar(data['day'], data['amount'],
+              totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending);
         }).toList(),
       ),
     );

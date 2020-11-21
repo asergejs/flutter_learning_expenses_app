128fb08625c835ebf0ed5cfb28e40a98c4b8a07e
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2020-11-21 18:37:44 +0200

    105 - Adding a ListTile Widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -27,37 +27,21 @@ class TransactionList extends StatelessWidget {
               //89
               itemBuilder: (ctx, idx) {
                 return Card(
-                    child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                          '\$ : ${transactions[idx].amount.toStringAsFixed(2)}',
-                          //90
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).primaryColor)),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Theme.of(context).primaryColor, width: 2)),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[idx].title,
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        Text(DateFormat.yMMMd().format(transactions[idx].date),
-                            style: TextStyle(fontSize: 12, color: Colors.grey))
-                      ],
-                    )
-                  ],
-                ));
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: FittedBox(
+                              child: Text('${transactions[idx].amount}')),
+                        )),
+                    title: Text(transactions[idx].title,
+                        style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(DateFormat.yMMMd().format(transactions[idx].date)),
+                  ),
+                );
               },
               itemCount: transactions.length,
             ),

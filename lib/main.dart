46ae21d7cfafbeea2e1611a6de5aeb5ff49179a8
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2020-12-26 11:01:39 +0200

    118 - Calculating sizes dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -91,23 +91,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Personal expenses'), //95
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context),
+        )
+      ],
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal expenses'), //95
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
-          )
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTransaction)
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height - MediaQuery.of(context).padding.top) *
+                    0.4,
+                child: Chart(_recentTransactions)),
+            Container(height: (MediaQuery.of(context).size.height -
+                appBar.preferredSize.height - MediaQuery.of(context).padding.top) *
+                0.6,
+                child: TransactionList(_userTransactions, _deleteTransaction))
           ],
         ),
       ),

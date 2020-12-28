5ed611b29148f8daee849dc6d5737db390432db3
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2020-12-28 16:58:47 +0200

    143 - Extracting Widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:expenses/models/transaction.dart';
+import 'package:expenses/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -29,35 +30,11 @@ class TransactionList extends StatelessWidget {
         : ListView.builder(
             //89
             itemBuilder: (ctx, idx) {
-              return Card(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                elevation: 5,
-                child: ListTile(
-                    leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(
-                              child: Text('${transactions[idx].amount}')),
-                        )),
-                    title: Text(transactions[idx].title,
-                        style: Theme.of(context).textTheme.headline6),
-                    subtitle:
-                        Text(DateFormat.yMMMd().format(transactions[idx].date)),
-                    trailing: MediaQuery.of(context).size.width > 360
-                        ? FlatButton.icon(
-                            icon: Icon(Icons.delete),
-                            textColor: Theme.of(context).errorColor,
-                            label: Text('Delete'),
-                            onPressed: () => deleteTx(transactions[idx].id))
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => deleteTx(transactions[idx].id),
-                          )),
-              );
+              return TransactionItem(transaction: transactions[idx], deleteTx: deleteTx);
             },
             itemCount: transactions.length,
           );
   }
 }
+
+
